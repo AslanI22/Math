23f35f637c3d4d1c4af9b537cb6d0886ca6c76a5
AslanI22 <126279604+AslanI22@users.noreply.github.com>
2023-03-04 18:07:43 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,2 +1,46 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Kalkulator
+{
+    class Bazar
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите первое число: ");
+            double x = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Введите дейсвие: ");
+            char z = Convert.ToChar(Console.ReadLine());
+
+            if ((z != '+') && (z != '-') && (z != '*') && (z != '/'))
+            {
+                Console.Write("Неправильно введено действие.");
+                return;
+            }
+            Console.Write("Введите второе число: ");
+            double y = Convert.ToInt32(Console.ReadLine());
+
+            switch (z)
+            {
+                case '+':
+                    Console.WriteLine($"Результат: {x} + {y} = " + (x + y));
+                    break;
+                case '-':
+                    Console.WriteLine($"Результат: {x} - {y} = " + (x - y));
+                    break;
+                case '*':
+                    Console.WriteLine($"Результат: {x} * {y} = " + (x * y));
+                    break;
+                case '/':
+                    if ((z == '/') && (y == 0))
+                    {
+                        Console.WriteLine("Ошибка, на ноль делить нельзя.");
+                        return;
+                    }
+                    Console.WriteLine($"Результат: {x} / {y} = " + (x / y));
+                    break;
+
+            }
+        }
+    }
+}
+
